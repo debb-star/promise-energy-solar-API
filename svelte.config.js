b65b3bce07c5772d0a90cc2fa24f2cc6f56ecbfa
debb-star <debangan@fractionally.co>
2024-12-29 16:10:40 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,9 @@
  limitations under the License.
  */
 
-import adapter from '@sveltejs/adapter-vercel'; // Changed to Vercel adapter
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -24,13 +25,14 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // Using the Vercel adapter for deployment
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
 
     // https://kit.svelte.dev/docs/configuration#version
     version: {
-      // Optional: If this causes issues, you can remove this property
-      name: process.env.VERCEL_GIT_COMMIT_SHA || 'development',
+      name: child_process.execSync('git rev-parse HEAD').toString().trim(),
     },
   },
 };
